c03b0c7a81e2c16ea2ba65aa95ebbdad9045f115
Louis Nguyen <tho.it@impc.vn>
2023-02-11 16:24:03 +0000

    implement http_request

---
+++ test.py
@@ -0,0 +1,13 @@
+from http_request import HttpRequest, default_cache_key_calculator
+from reactivex import operators as ops
+
+import logging
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+    
+if __name__ == '__main__':
+    request = HttpRequest(cache_key_calculator=default_cache_key_calculator)
+    response1 = request.get_response_stream("https://www.google.com")
+    response1.pipe(ops.map(lambda res: None)).subscribe()
+    
+    response2 = request.get_response_stream("https://www.google.com")
+    response2.pipe(ops.map(lambda res: None)).subscribe()
