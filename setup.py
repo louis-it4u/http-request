b728c3c8264a658542a634f07a7eb1125f7bb15e
Louis Nguyen <tho.it@impc.vn>
2023-02-11 16:56:20 +0000

    update setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name = 'http_request',
     packages = ['http_request'],
-    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
-    description = 'description',
+    version = 'v1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'HttpRequest will return response in cache if exists, else it will open request',
     author = 'Louis',
     author_email = 'thonh.it@gmail.com',
     url = 'https://github.com/louis-it4u/http-request',
